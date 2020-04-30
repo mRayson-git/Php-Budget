6792a1f4240bc0e37fb1c7250a1e46d896f9bb78
Michael Rayson <mrayson5129@gmail.com>
2020-04-30 13:38:40 -0400

    Fixing DB connection p1

--- website/Views/login.php
+++ website/Views/login.php
@@ -15,6 +15,7 @@
         <div class="login">
           <?php
           try{
+            ini_set('display_errors', 1);
             session_start();
             include('../Models/userDAO.php');
             $userDAO = new userDAO();
