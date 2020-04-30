cbac7c0d0c66537a555c3dc3a8589fbfe0cf8bc1
Michael Rayson <mrayson5129@gmail.com>
2020-04-30 13:29:29 -0400

    Hidden database password

--- website/Models/abstractDAO.php
+++ website/Models/abstractDAO.php
@@ -3,12 +3,12 @@ class abstractDAO{
   protected $mysqli;
   protected static $DB_HOST = "localhost";
   protected static $DB_USERNAME = "budgetAdmin";
-  protected static $DB_PASSWORD = "thinkBudg3tThinkMon3y";
   protected static $DB_DATABASE = "budgetApplication";
 
   function __construct(){
     try{
-      $this->mysqli = new mysqli(self::$DB_HOST, self::$DB_USERNAME, self::$DB_PASSWORD, self::$DB_DATABASE);
+
+      $this->mysqli = new mysqli(self::$DB_HOST, self::$DB_USERNAME, json_decode(file_get_contents('./databaseLoginInfo.json'), true)[0], self::$DB_DATABASE);
     }catch(mysqli_sql_exception $e){
       throw $e;
     }
